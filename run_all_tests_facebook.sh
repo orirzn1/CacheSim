541a54487e3bac847627fea8923f02425cdb267e
orirzn1 <117581392+orirzn1@users.noreply.github.com>
2024-03-10 21:30:54 +0200

    Update run_all_tests_facebook.sh

--- run_all_tests_facebook.sh
+++ run_all_tests_facebook.sh
@@ -5,12 +5,12 @@ echo The test succeed if there are no diffs printed.
 echo
 all=0
 fail=0
-for filename in tests/test*.command; do
+for filename in tests/tests/test*.command; do
     test_num=`echo $filename | cut -d'.' -f1`
      bash ${filename} > ${test_num}.YoursOut
 done
 
-for filename in tests/test*.out; do
+for filename in tests/tests/test*.out; do
 	(( all++ ))
     test_num=`echo $filename | cut -d'.' -f1`
     diff_result=$(diff ${test_num}.out ${test_num}.YoursOut)
